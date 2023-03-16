4d6cf9b49748b13d0d41b8d4e5005034f7e25bf2
Emmanuel Valverde Ramos <evrtrabajo@gmail.com>
2023-03-16 08:39:30 +0100

    red: given a 2 should return a 2

--- 1 The Physical/Guess Point Level 1 (Stateless)/1 - Red-Green-Refactor Practice/1.1 - Fizzbuzz/src/index.spec.ts
+++ 1 The Physical/Guess Point Level 1 (Stateless)/1 - Red-Green-Refactor Practice/1.1 - Fizzbuzz/src/index.spec.ts
@@ -6,4 +6,8 @@ describe("fizzbuzz algorithm", () => {
     it('given a 1 should return a 1', () => {
         expect(fizzbuzz(1)).toBe("1");
     })
+
+    it('given a 2 should return a 2', () => {
+        expect(fizzbuzz(2)).toBe("2");
+    })
 });
