77923cd1ec1016977cc21d1dd196405d82ae8b06
Emmanuel Valverde Ramos <evrtrabajo@gmail.com>
2023-03-16 08:47:09 +0100

    refactor: deleting old test cover on parameterized test

--- 1 The Physical/Guess Point Level 1 (Stateless)/1 - Red-Green-Refactor Practice/1.1 - Fizzbuzz/src/index.spec.ts
+++ 1 The Physical/Guess Point Level 1 (Stateless)/1 - Red-Green-Refactor Practice/1.1 - Fizzbuzz/src/index.spec.ts
@@ -10,16 +10,4 @@ describe("fizzbuzz algorithm", () => {
     ])('given a %s should return a %s', (inputNumber: number, expectedOutput: string) => {
         expect(fizzbuzz(inputNumber)).toBe(expectedOutput);
     });
-
-    it('given a 1 should return a 1', () => {
-        expect(fizzbuzz(1)).toBe("1");
-    });
-
-    it('given a 2 should return a 2', () => {
-        expect(fizzbuzz(2)).toBe("2");
-    });
-
-    it('given a 4 should return a 4', () => {
-        expect(fizzbuzz(4)).toBe("4");
-    });
 });
