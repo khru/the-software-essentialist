9c7c1d775e1d003577caaef91ad7c43d4300ef87
Emmanuel Valverde Ramos <evrtrabajo@gmail.com>
2023-03-16 08:57:06 +0100

    refactor: test to encapsulate each context and parameterized test

--- 1 The Physical/Guess Point Level 1 (Stateless)/1 - Red-Green-Refactor Practice/1.1 - Fizzbuzz/src/index.spec.ts
+++ 1 The Physical/Guess Point Level 1 (Stateless)/1 - Red-Green-Refactor Practice/1.1 - Fizzbuzz/src/index.spec.ts
@@ -5,24 +5,25 @@ function fizzbuzz(number: number): string {
     return String(number);
 }
 
-describe("fizzbuzz algorithm", () => {
-    it.each([
-        [1, "1"],
-        [2, "2"],
-        [4, "4"],
-    ])('given a %s should return a %s', (inputNumber: number, expectedOutput: string) => {
-        expect(fizzbuzz(inputNumber)).toBe(expectedOutput);
+describe("Given a number the fizzbuzz algorithm", () => {
+
+    describe("should return the same numbers when they are not multiples of three nor five", () => {
+        it.each([
+            [1, "1"],
+            [2, "2"],
+            [4, "4"],
+        ])('given a %s should return a %s', (inputNumber: number, expectedOutput: string) => {
+            expect(fizzbuzz(inputNumber)).toBe(expectedOutput);
+        });
     });
 
-    it('given a 3 should return a Fizz', () => {
-        expect(fizzbuzz(3)).toBe("Fizz");
-    });
-
-    it('given a 6 should return a Fizz', () => {
-        expect(fizzbuzz(6)).toBe("Fizz");
-    });
-
-    it('given a 9 should return a Fizz', () => {
-        expect(fizzbuzz(9)).toBe("Fizz");
+    describe("should return Fizz when they are multiples of three", () => {
+        it.each([
+            [3],
+            [6],
+            [9],
+        ])('given a %s should return a Fizz', (inputNumber: number) => {
+            expect(fizzbuzz(inputNumber)).toBe("Fizz");
+        });
     });
 });
