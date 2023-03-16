2ae828a4fc13206315605dc1156d446de9b319c9
Emmanuel Valverde Ramos <evrtrabajo@gmail.com>
2023-03-16 08:46:32 +0100

    refactor: adding new parameterized test without deleting old yet

--- 1 The Physical/Guess Point Level 1 (Stateless)/1 - Red-Green-Refactor Practice/1.1 - Fizzbuzz/src/index.spec.ts
+++ 1 The Physical/Guess Point Level 1 (Stateless)/1 - Red-Green-Refactor Practice/1.1 - Fizzbuzz/src/index.spec.ts
@@ -3,15 +3,23 @@ function fizzbuzz(number: number): string {
 }
 
 describe("fizzbuzz algorithm", () => {
+    it.each([
+        [1, "1"],
+        [2, "2"],
+        [4, "4"],
+    ])('given a %s should return a %s', (inputNumber: number, expectedOutput: string) => {
+        expect(fizzbuzz(inputNumber)).toBe(expectedOutput);
+    });
+
     it('given a 1 should return a 1', () => {
         expect(fizzbuzz(1)).toBe("1");
-    })
+    });
 
     it('given a 2 should return a 2', () => {
         expect(fizzbuzz(2)).toBe("2");
-    })
+    });
 
     it('given a 4 should return a 4', () => {
         expect(fizzbuzz(4)).toBe("4");
-    })
+    });
 });
