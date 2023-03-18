36add65ab8c8136ce546475771aff2e44a077498
Emmanuel Valverde Ramos <evrtrabajo@gmail.com>
2023-03-18 20:07:37 +0100

    refactor: beforeEach removing static and adding constants to add semantics

--- 1 The Physical/Guess Point Level 1 (Stateless)/1 - Red-Green-Refactor Practice/1.2 - Palindrome/src/index.spec.ts
+++ 1 The Physical/Guess Point Level 1 (Stateless)/1 - Red-Green-Refactor Practice/1.2 - Palindrome/src/index.spec.ts
@@ -1,11 +1,16 @@
 import { Palindrome } from './palindrome';
 
 describe('palindrome checker', () => {
+    let palindromeChecker: Palindrome;
+    beforeEach(() => {
+        palindromeChecker = new Palindrome();
+    });
+
     it.each([
         ['A totally random example'],
         ['123456'],
     ])('should return true when call with {%s} palindrome',(nonPalindrome: string) => {
-        expect(Palindrome.check(nonPalindrome)).toBeFalsy();
+        expect(palindromeChecker.check(nonPalindrome)).toBeFalsy();
     });
 
     it.each([
@@ -17,6 +22,6 @@ describe('palindrome checker', () => {
         ['Was It A Rat I Saw'],
         ['Never Odd or Even'],
     ])('should return true when call with {%s} palindrome',(palindrome: string) => {
-        expect(Palindrome.check(palindrome)).toBeTruthy();
+        expect(palindromeChecker.check(palindrome)).toBeTruthy();
     });
 })
--- 1 The Physical/Guess Point Level 1 (Stateless)/1 - Red-Green-Refactor Practice/1.2 - Palindrome/src/palindrome.ts
+++ 1 The Physical/Guess Point Level 1 (Stateless)/1 - Red-Green-Refactor Practice/1.2 - Palindrome/src/palindrome.ts
@@ -1,8 +1,12 @@
 export class Palindrome {
-    public static check(word: string): boolean {
-        return Palindrome.cleanWord(word) === Palindrome.cleanWord(word).split('').reverse().join('');
+    private readonly EACH_CHARTER = '';
+    private readonly EMPTY_CHARACTER = '';
+    private readonly SPACES_REGEX = / /g;
+
+    public check(word: string): boolean {
+        return this.cleanWord(word) === this.cleanWord(word).split(this.EACH_CHARTER).reverse().join(this.EACH_CHARTER);
     }
-    private static cleanWord(word: string): string {
-        return word.toLowerCase().replace(/ /g, '')
+    private cleanWord(word: string): string {
+        return word.toLowerCase().replace(this.SPACES_REGEX, this.EMPTY_CHARACTER)
     }
 }
