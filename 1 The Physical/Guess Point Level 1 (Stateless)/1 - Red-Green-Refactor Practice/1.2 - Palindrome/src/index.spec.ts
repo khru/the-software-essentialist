bc12901f2d3e85db41c57c30329fd44a583ad72d
Emmanuel Valverde Ramos <evrtrabajo@gmail.com>
2023-03-18 19:36:01 +0100

    green: should return true when call with {non} palindrome

--- 1 The Physical/Guess Point Level 1 (Stateless)/1 - Red-Green-Refactor Practice/1.2 - Palindrome/src/index.spec.ts
+++ 1 The Physical/Guess Point Level 1 (Stateless)/1 - Red-Green-Refactor Practice/1.2 - Palindrome/src/index.spec.ts
@@ -1,9 +1,16 @@
 function palindromeChecker(word: string): boolean {
+    if (word === 'non') {
+        return true;
+    }
     return false;
 }
 
 describe('palindrome checker', () => {
     it('should return false when call with a non palindrome',  () => {
         expect(palindromeChecker('A totally random example')).toBeFalsy();
+    });
+
+    it('should return true when call with {non} palindrome',  () => {
+        expect(palindromeChecker('non')).toBeTruthy();
     });
 })
