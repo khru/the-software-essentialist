41a58689c55f72a25090a8782809c35fa29e22f0
Emmanuel Valverde Ramos <evrtrabajo@gmail.com>
2023-03-20 20:14:13 +0100

    refactor: change passwords to meet the 15 chart expectation

--- 1 The Physical/Guess Point Level 1 (Stateless)/2 - Arrange-Act-Assert Backwards/2.1 - Password Validator/src/index.spec.ts
+++ 1 The Physical/Guess Point Level 1 (Stateless)/2 - Arrange-Act-Assert Backwards/2.1 - Password Validator/src/index.spec.ts
@@ -5,9 +5,14 @@ function hasUppercase(password: string) {
     return password.match(uppercasePatternRegex) === null;
 }
 
+function hasMinLength(password: string) {
+    const MIN_LENGTH_FOR_A_PASSWORD = 5;
+    return password.length < MIN_LENGTH_FOR_A_PASSWORD;
+}
+
 function passwordValidator(password: string): PasswordValidation {
 
-    if (password.length < 5) {
+    if (hasMinLength(password)) {
         return { status: false, errors: ['TOO_SHORT']};
     }
 
@@ -21,7 +26,7 @@ function passwordValidator(password: string): PasswordValidation {
 describe('password validator', () => {
     it('given the password {A non valid password} when call the password validator then it should return an invalid response with NO_DIGITS error', () => {
         // Given
-        const A_PASSWORD_WITHOUT_DIGITS = 'A non valid password';
+        const A_PASSWORD_WITHOUT_DIGITS = 'A non valid';
         // When
         const result = passwordValidator(A_PASSWORD_WITHOUT_DIGITS);
 
@@ -31,9 +36,9 @@ describe('password validator', () => {
     });
 
     it.each([
-        ['1 non valid password'],
-        ['2 non valid password'],
-        ['3 non valid password'],
+        ['1 non valid'],
+        ['2 non valid'],
+        ['3 non valid'],
     ])('given the password {%s} when call the password validator then it should return an invalid response with NO_UPPER error', (invalidPassword: string) => {
         const result = passwordValidator(invalidPassword);
         expect(result.status).toBeFalsy()
