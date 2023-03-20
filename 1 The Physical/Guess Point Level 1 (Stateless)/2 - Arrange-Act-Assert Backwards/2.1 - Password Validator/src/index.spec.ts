27900a3417384e771c187ac0d9c825ada9b246c6
Emmanuel Valverde Ramos <evrtrabajo@gmail.com>
2023-03-20 18:44:05 +0100

    green: given the password {1 non valid password} when call the password validator then it should return an invalid response with NO_UPPER error

--- 1 The Physical/Guess Point Level 1 (Stateless)/2 - Arrange-Act-Assert Backwards/2.1 - Password Validator/src/index.spec.ts
+++ 1 The Physical/Guess Point Level 1 (Stateless)/2 - Arrange-Act-Assert Backwards/2.1 - Password Validator/src/index.spec.ts
@@ -1,11 +1,15 @@
 type PasswordValidation = { status: boolean, errors: Array<string> };
 
 function passwordValidator(password: string): PasswordValidation {
+    if (password === '1 non valid password') {
+        return { status: false, errors: ['NO_UPPER']};
+    }
+
     return { status: false, errors: ['NO_DIGITS']};
 }
 
 describe('password validator', () => {
-    it('given the password {A non valid password} when call the password validator then it should return an invalid response with NON_DIGITS error', () => {
+    it('given the password {A non valid password} when call the password validator then it should return an invalid response with NO_DIGITS error', () => {
         // Given
         const A_PASSWORD_WITHOUT_DIGITS = 'A non valid password';
         // When
@@ -14,6 +18,17 @@ describe('password validator', () => {
         // Then
         expect(result.status).toBeFalsy()
         expect(result.errors).toEqual(['NO_DIGITS'])
+    });
+
+    it('given the password {1 non valid password} when call the password validator then it should return an invalid response with NO_UPPER error', () => {
+        // Given
+        const A_PASSWORD_WITHOUT_UPPER = '1 non valid password';
+        // When
+        const result = passwordValidator(A_PASSWORD_WITHOUT_UPPER);
+
+        // Then
+        expect(result.status).toBeFalsy()
+        expect(result.errors).toEqual(['NO_UPPER'])
     });
 })
 
