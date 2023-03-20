56d47955c45b756cb12a02d706dc5892f9ab537c
Emmanuel Valverde Ramos <evrtrabajo@gmail.com>
2023-03-20 07:52:24 +0100

    green: given the password {A non valid password} when call the password validator then it should return an invalid response with NON_DIGITS error

--- 1 The Physical/Guess Point Level 1 (Stateless)/2 - Arrange-Act-Assert Backwards/2.1 - Password Validator/src/index.spec.ts
+++ 1 The Physical/Guess Point Level 1 (Stateless)/2 - Arrange-Act-Assert Backwards/2.1 - Password Validator/src/index.spec.ts
@@ -1,5 +1,19 @@
+type PasswordValidation = { status: boolean, errors: Array<string> };
+
+function passwordValidator(password: string): PasswordValidation {
+    return { status: false, errors: ['NO_DIGITS']};
+}
 
 describe('password validator', () => {
+    it('given the password {A non valid password} when call the password validator then it should return an invalid response with NON_DIGITS error', () => {
+        // Given
+        const A_PASSWORD_WITHOUT_DIGITS = 'A non valid password';
+        // When
+        const result = passwordValidator(A_PASSWORD_WITHOUT_DIGITS);
 
+        // Then
+        expect(result.status).toBeFalsy()
+        expect(result.errors).toEqual(['NO_DIGITS'])
+    });
 })
 
