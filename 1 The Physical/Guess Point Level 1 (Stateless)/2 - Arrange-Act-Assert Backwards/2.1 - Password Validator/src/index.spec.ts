ecebd5aea5114ee7af43d3e8b10d0a29c69797ce
Emmanuel Valverde Ramos <evrtrabajo@gmail.com>
2023-03-20 20:05:04 +0100

    green: given a short password {B2fe} when call the password validator then it should return an invalid response with TOO_SHORT error

--- 1 The Physical/Guess Point Level 1 (Stateless)/2 - Arrange-Act-Assert Backwards/2.1 - Password Validator/src/index.spec.ts
+++ 1 The Physical/Guess Point Level 1 (Stateless)/2 - Arrange-Act-Assert Backwards/2.1 - Password Validator/src/index.spec.ts
@@ -11,6 +11,10 @@ function passwordValidator(password: string): PasswordValidation {
         return { status: false, errors: ['TOO_SHORT']};
     }
 
+    if (password === 'B2fe') {
+        return { status: false, errors: ['TOO_SHORT']};
+    }
+
     if (hasUppercase(password)) {
         return { status: false, errors: ['NO_UPPER']};
     }
@@ -43,6 +47,17 @@ describe('password validator', () => {
     it('given a short password {A1fe} when call the password validator then it should return an invalid response with TOO_SHORT error', () => {
         // Given
         const A_TOO_SHORT_PASSWORD = 'A1fe';
+        // When
+        const result = passwordValidator(A_TOO_SHORT_PASSWORD);
+
+        // Then
+        expect(result.status).toBeFalsy()
+        expect(result.errors).toEqual(['TOO_SHORT'])
+    });
+
+    it('given a short password {B2fe} when call the password validator then it should return an invalid response with TOO_SHORT error', () => {
+        // Given
+        const A_TOO_SHORT_PASSWORD = 'B2fe';
         // When
         const result = passwordValidator(A_TOO_SHORT_PASSWORD);
 
