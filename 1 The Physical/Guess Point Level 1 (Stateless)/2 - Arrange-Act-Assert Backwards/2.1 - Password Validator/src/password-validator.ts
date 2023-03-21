bc1b0781873f7beaab5475c0b0fc2b10dc2ab4a7
Emmanuel Valverde Ramos <evrtrabajo@gmail.com>
2023-03-21 14:59:38 +0100

    feedback: change name of the validations

--- 1 The Physical/Guess Point Level 1 (Stateless)/2 - Arrange-Act-Assert Backwards/2.1 - Password Validator/src/password-validator.ts
+++ 1 The Physical/Guess Point Level 1 (Stateless)/2 - Arrange-Act-Assert Backwards/2.1 - Password Validator/src/password-validator.ts
@@ -9,42 +9,42 @@ export class PasswordValidator {
             errors: []
         }
 
-        if (this.hasAtLeastANumber(password)) {
+        if (this.doesNotHaveAtLeastANumber(password)) {
             passwordValidation.status = false;
             passwordValidation.errors.push(Errors.NO_DIGITS);
         }
 
-        if (this.hasMaxLength(password)) {
+        if (this.doesNotMaxLength(password)) {
             passwordValidation.status = false;
             passwordValidation.errors.push(Errors.TOO_LONG);
         }
 
-        if (this.hasMinLength(password)) {
+        if (this.doesNotHaveMinLength(password)) {
             passwordValidation.status = false;
             passwordValidation.errors.push(Errors.TOO_SHORT);
         }
 
-        if (this.hasUppercase(password)) {
+        if (this.doesNotHaveUppercase(password)) {
             passwordValidation.status = false;
             passwordValidation.errors.push(Errors.NO_UPPER);
         }
 
         return passwordValidation;
     }
-    private hasUppercase(password: string): boolean {
+    private doesNotHaveUppercase(password: string): boolean {
         const uppercasePatternRegex: RegExp = /.*[A-Z].*/g;
         return password.match(uppercasePatternRegex) === null;
     }
-    private hasMinLength(password: string):boolean {
+    private doesNotHaveMinLength(password: string):boolean {
         const MIN_LENGTH_FOR_A_PASSWORD = 5;
         return password.length < MIN_LENGTH_FOR_A_PASSWORD;
     }
-    private hasMaxLength(password: string): boolean {
+    private doesNotMaxLength(password: string): boolean {
         const MAX_LENGTH_FOR_A_PASSWORD = 15;
         return password.length > MAX_LENGTH_FOR_A_PASSWORD;
     }
 
-    private hasAtLeastANumber(password: string): boolean {
+    private doesNotHaveAtLeastANumber(password: string): boolean {
         const atLeastANumberPatternRegex: RegExp = /.*[0-9]+.*/g;
         return password.match(atLeastANumberPatternRegex) === null;
     }
