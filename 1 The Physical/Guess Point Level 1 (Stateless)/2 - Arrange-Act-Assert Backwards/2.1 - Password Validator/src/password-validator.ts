9c2af4ce937e86ccb5db8c300f0aa8e6770a3474
Emmanuel Valverde Ramos <evrtrabajo@gmail.com>
2023-03-21 07:28:24 +0100

    refactor: add hasMinLength validation to class

--- 1 The Physical/Guess Point Level 1 (Stateless)/2 - Arrange-Act-Assert Backwards/2.1 - Password Validator/src/password-validator.ts
+++ 1 The Physical/Guess Point Level 1 (Stateless)/2 - Arrange-Act-Assert Backwards/2.1 - Password Validator/src/password-validator.ts
@@ -11,7 +11,7 @@ export class PasswordValidator {
             return { status: false, errors: ['TOO_LONG']};
         }
 
-        if (hasMinLength(password)) {
+        if (this.hasMinLength(password)) {
             return { status: false, errors: ['TOO_SHORT']};
         }
 
@@ -25,18 +25,11 @@ export class PasswordValidator {
         const uppercasePatternRegex: RegExp = /.*[A-Z].*/g;
         return password.match(uppercasePatternRegex) === null;
     }
+    private hasMinLength(password: string):boolean {
+        const MIN_LENGTH_FOR_A_PASSWORD = 5;
+        return password.length < MIN_LENGTH_FOR_A_PASSWORD;
+    }
 }
-
-function hasUppercase(password: string) {
-    const uppercasePatternRegex: RegExp = /.*[A-Z].*/g;
-    return password.match(uppercasePatternRegex) === null;
-}
-
-function hasMinLength(password: string) {
-    const MIN_LENGTH_FOR_A_PASSWORD = 5;
-    return password.length < MIN_LENGTH_FOR_A_PASSWORD;
-}
-
 function hasMaxLength(password: string) {
     const MAX_LENGTH_FOR_A_PASSWORD = 15;
     return password.length > MAX_LENGTH_FOR_A_PASSWORD;
